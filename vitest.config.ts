b274cecf7d711fa8d89c20b55a4d3ff1fdb93a82
huylee3 <huyleee.dev@gmail.com>
2026-09-24 18:05:12 -0700

    ci(feat): test report

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    reporters: [
+				'github-actions', { displayAnnotation: false}]
+		],
     fileParallelism: false,
     projects: [
       {
